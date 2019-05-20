b75fb08fef70a412e5530f7e207a7ffa438e5c73
Chris Stopford <c.stopford@herts.ac.uk>
2019-05-20 18:42:14 +0100

    Separate series by colour

--- GetTvRatings.py
+++ GetTvRatings.py
@@ -23,14 +23,25 @@ nseasonsStr = nseasonsStr[:nseasonsStr.find('\xa0')]
 
 # Find season title
 stitle = ssoup.find('title').text.replace(' - IMDb','')
-print(stitle)
+print(stitle + ', ' + nseasonsStr + ' seasons')
 
 
 # Get ratings:
 ratings = []  
 votes = []    
 
+# Number the episodes so we can plot different colours etc
+seriesNumber = []
+episodeNumber = []
+RunningEpisodeNumber = 0
+FirstEpisodeOfSeries = 0
+maxVotes = 0
+
+# Create figure
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
 for i in range(1, int(nseasonsStr)+1):
+    FirstEpisodeOfSeries = -1
 
     print('  Season: ' + str(i))
     url = 'http://www.imdb.com/title/' + imdbID + '/episodes?season=' + str(i)
@@ -42,23 +53,53 @@ for i in range(1, int(nseasonsStr)+1):
 
     sections = soup.find_all('div', attrs={'class':'ipl-rating-star'})
 
-
     for section in sections:
         if len(section) == 7:
+            # Running Episode Number
+            RunningEpisodeNumber = RunningEpisodeNumber + 1
+            
+            # First episode of season
+            if FirstEpisodeOfSeries < 0:
+                FirstEpisodeOfSeries = RunningEpisodeNumber
+
+            # Get episode rating
             valString = section.text.lstrip()
-            ratings.append(float(valString[:valString.find('\n')]))
+            rating = float(valString[:valString.find('\n')])
+            ratings.append(rating)
+            
+            # Get episode number of votes
             vote = valString[valString.find('(') + 1 : valString.find(')')].replace(',','')
-            votes.append(float(vote))    
-        
+            votes.append(float(vote)) 
+            if float(vote)>maxVotes:
+                maxVotes=float(vote)   
+
+
+    
+    print('First episode number: '+ str(FirstEpisodeOfSeries))
+    print('Running episode number: ' + str(RunningEpisodeNumber))
+    print('Length of ratings: ' + str(len(ratings)))
+    print(' ')
+    # Plot
+    x = range(FirstEpisodeOfSeries, len(ratings) + FirstEpisodeOfSeries)
+    ax1.scatter(x, ratings)
+    ax2.scatter(x, votes)
+            
+    # Reset episode stuff
+    ratings.clear()
+    votes.clear()
+
+
+
 # Plot output        
 x = range(0,len(ratings))
-f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-ax1.scatter(x, ratings)
 ax1.set_title(stitle)
+ax1.set_ylabel('Rating')
+ax1.set
 ax1.set_ylim(0, 10)
 ax1.grid()
-ax2.scatter(x, votes)
-ax2.set_ylim(0, max(votes)*1.2)
+
+ax2.set_ylim(0, maxVotes*1.2)
+ax2.set_ylabel('# Votes')
 ax2.grid()
 plt.savefig(imdbID + '.png')
 plt.show()
